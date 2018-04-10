ce115680c82260bee9cdab39b4513b7561d4d7a2
Neil Hiddink <nhiddink2@gmail.com>
2018-04-10 12:00:36 -0400

    Initial Commit

---
+++ WatchConnectivityDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchConnectivityDemo
+//
+//  Created by Neil Hiddink on 4/10/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
