61f2bf705f17099810ba1153331d36317c86b4ec
Neil Hiddink <nhiddink2@gmail.com>
2018-04-10 12:22:45 -0400

    feat: Add UI Shell and Connect IB Actions in Interface.storyboard.

--- WatchConnectivityDemo WatchKit Extension/InterfaceController.swift
+++ WatchConnectivityDemo WatchKit Extension/InterfaceController.swift
@@ -15,17 +15,30 @@ class InterfaceController: WKInterfaceController {
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
     }
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+    // MARK: Helper Methods
+    
+    func buttonPressed(name: String) {
+        print("Button Pressed: \(name)")
     }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    // MARK: IB Actions
+    
+    @IBAction func heartsButtonPressed() {
+        buttonPressed(name: "Hearts")
     }
-
+    
+    @IBAction func spadesButtonPressed() {
+        buttonPressed(name: "Spades")
+    }
+    
+    @IBAction func clubsButtonPressed() {
+        buttonPressed(name: "Clubs")
+    }
+    
+    @IBAction func diamondsButtonPressed() {
+        buttonPressed(name: "Diamonds")
+    }
+    
 }
